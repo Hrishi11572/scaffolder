8904c1fdafc09c6bdd294f1f5084937caa4ce5d8
Hrishikesh Tiwari <tiwarihrishikesh686@gmail.com>
2026-08-22 21:33:27 +0530

    updated generator file

--- src/scaffolder/generator.py
+++ src/scaffolder/generator.py
@@ -1,5 +1,6 @@
 from pathlib import Path 
 from model import Node, NodeType
+from templates import get_template
 
 def generator(root : Node, target): 
     root_path = Path(target)
@@ -7,8 +8,14 @@ def generator(root : Node, target):
     if root.type == NodeType.DIRECTORY: 
         root_path.mkdir(parents=True, exist_ok=True)
     else: 
-        root_path.touch()
-    
+        template = get_template(root.name)
+        
+        if template is not None: 
+            root_path.write_text(template) 
+        else: 
+            root_path.touch()
+        
+        
     for child in root.children: 
         node_path = root_path /  child.name
         generator(child, node_path)
