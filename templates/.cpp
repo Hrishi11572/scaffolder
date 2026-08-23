32f707392615cc5ab4cb740bddc430d8c5562392
Hrishikesh Tiwari <tiwarihrishikesh686@gmail.com>
2026-08-23 11:03:49 +0530

    changed test file names

--- templates/.cpp
+++ templates/.cpp
@@ -2,6 +2,7 @@
 using namespace std; 
 
 int main(){
+    cout << "This is default template" << endl; 
     cout << "Hello World!" << endl; 
     return 0; 
 }
--- templates/.py
+++ templates/.py
@@ -2,5 +2,6 @@ def main():
     pass 
 
 if __name__ == "__main__": 
+    print("This is a default template file")
     print("Hello, this is python template")
     main()
