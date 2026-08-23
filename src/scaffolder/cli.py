f1f1cf0357c2c2456cac0a9d2685f2f5b27d336e
Hrishikesh Tiwari <tiwarihrishikesh686@gmail.com>
2026-08-23 11:25:04 +0530

    added error handling in cli setup

--- src/scaffolder/cli.py
+++ src/scaffolder/cli.py
@@ -6,10 +6,26 @@ from pathlib import Path
 
 
 def build(markdown_file: str, target: str, templates : str | None = None):
-    markdown = Path(markdown_file).read_text()
+    
+    markdown_path = Path(markdown_file)
+    
+    # error handling here 
+    if not markdown_path.exists(): 
+        raise typer.BadParameter(
+            f'Markdown file {markdown_file} does not exist.'
+        )
+        
+    markdown = markdown_path.read_text()
     
     structure = extract_structure(markdown)
     root = parse_structure(structure)
+    
+    # error handling here 
+    if root is None: 
+        raise typer.BadParameter(
+            "Could not parse the directory structure."
+        )
+    
     generator(root, target, templates)
 
 if __name__ == "__main__":
